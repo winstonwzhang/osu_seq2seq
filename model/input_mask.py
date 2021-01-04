bba7355b45d978af8d95d14832de536c168bf458
Winston Zhang <wwzhang@umich.edu>
2021-01-04 01:49:14 -0500

    try masking empty tokens

--- model/input_mask.py
+++ model/input_mask.py
@@ -52,9 +52,9 @@ def create_masks(inp, tar):
     # Used in the 1st attention block in the decoder.
     # It is used to pad and mask future tokens in the input received by the decoder.
     look_ahead_mask = create_look_ahead_mask(tf.shape(tar)[1])
-    #dec_target_padding_mask = create_padding_mask(tar)
-    #combined_mask = tf.maximum(dec_target_padding_mask, look_ahead_mask)
-    combined_mask = look_ahead_mask
+    dec_target_padding_mask = create_padding_mask(tar)
+    combined_mask = tf.maximum(dec_target_padding_mask, look_ahead_mask)
+    #combined_mask = look_ahead_mask
 
     # print('enc_padding_mask',enc_padding_mask)
     # print('combined_mask', combined_mask)
