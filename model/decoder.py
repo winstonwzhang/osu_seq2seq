8be3d2cb35293041ec4edfd6f76097c215727bc1
Winston Zhang <wwzhang@umich.edu>
2021-01-05 00:22:04 -0500

    readd dec

--- model/decoder.py
+++ model/decoder.py
@@ -30,8 +30,8 @@ class Decoder(tf.keras.Model):
         # try just one-hot pos encoding
         #self.pos_encoding = tf.one_hot(np.arange(pe_max_len),depth=self.d_model)
 
-        #self.dec_layers = [DecoderLayer(d_model, num_heads, dff, 'DE'+str(_),rate)
-        #                   for _ in range(num_layers)]
+        self.dec_layers = [DecoderLayer(d_model, num_heads, dff, 'DE'+str(_),rate)
+                           for _ in range(num_layers)]
         self.dropout = tf.keras.layers.Dropout(rate,name='de_emb_dp')
 
         self.final_layer = tf.keras.layers.Dense(target_vocab_size)
@@ -56,11 +56,11 @@ class Decoder(tf.keras.Model):
         x = self.dropout(x, training=training)
         
         x = x + enc_output
-        #for i in range(self.num_layers):
-        #    x, block1, block2 = self.dec_layers[i](x, enc_output, training,
-        #                                           look_ahead_mask, padding_mask)
-        #    attention_weights['decoder_layer{}_block1'.format(i + 1)] = block1
-        #    attention_weights['decoder_layer{}_block2'.format(i + 1)] = block2
+        for i in range(self.num_layers):
+            x, block1, block2 = self.dec_layers[i](x, enc_output, training,
+                                                   look_ahead_mask, padding_mask)
+            attention_weights['decoder_layer{}_block1'.format(i + 1)] = block1
+            attention_weights['decoder_layer{}_block2'.format(i + 1)] = block2
 
         # before softmax
         x = self.final_layer(x)
